b323dcf4658f4fdf18e2223b6bda457f13bd4ef5
drzhangg <654014730@qq.com>
2022-02-09 14:35:57 +0800

    add api

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@ ls
 cat pwd.txt  | docker login -u drzhangg --password-stdin
 docker build -t drzhangg/webhook:latest .
 docker push drzhangg/webhook:latest
+
+docker
--- main.go
+++ main.go
@@ -16,12 +16,22 @@ func main() {
 		fmt.Println("接收到webhook请求")
 	})
 
-
 	r.POST("/post", func(c *gin.Context) {
-		fmt.Println("header:",c.Request.Header)
-		fmt.Println("Body:",c.Request.Body)
+		fmt.Println("header:", c.Request.Header)
+		fmt.Println("Body:", c.Request.Body)
 	})
 
+	r.GET("/info", func(c *gin.Context) {
+		username := c.Query("username")
+
+		if username != "" {
+			c.JSON(200, map[string]interface{}{
+				"name":    username,
+				"message": "hello," + username,
+			})
+			return
+		}
+	})
 
 	r.Run(":8989")
 }
